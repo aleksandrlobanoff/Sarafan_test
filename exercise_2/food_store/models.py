b4ad6d57d6c0fa4ca360d0bb6d7b5bfce67b3553
alexandr.lobanov <alexandp83@mail.ru>
2024-03-05 21:59:28 +0300

    Создана модель Категорий

--- exercise_2/food_store/models.py
+++ exercise_2/food_store/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Категория")
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='category', blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
