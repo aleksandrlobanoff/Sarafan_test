b57836fa812f5802fd3fb382d4c09e98449a3e57
alexandr.lobanov <alexandp83@mail.ru>
2024-03-05 21:59:58 +0300

    Создана модель Подкатегорий

--- exercise_2/food_store/models.py
+++ exercise_2/food_store/models.py
@@ -12,3 +12,17 @@ class Category(models.Model):
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
+
+
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='subcategory', blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Подкатегория"
+        verbose_name_plural = "Подкатегории"
