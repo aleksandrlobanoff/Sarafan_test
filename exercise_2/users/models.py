6afb144d896e9afb10ce86a4bab74668b6355df3
alexandr.lobanov <alexandp83@mail.ru>
2024-03-05 21:48:56 +0300

    Создана кастомная модель юзера

---
+++ exercise_2/users/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Создание пользовательской модели CustomUser
+class CustomUser(AbstractUser):
+    # Поле username для имени пользователя
+    username = models.CharField(max_length=150, default='')
+    # Поле name для имени пользователя
+    name = models.CharField(max_length=255)
+    # Поле email для адреса электронной почты пользователя
+    email = models.EmailField(unique=True)
+
+    # Задаем поле email как основное поле для входа в систему
+    USERNAME_FIELD = 'email'
+    # Требуем указание поля username при создании пользователя
+    REQUIRED_FIELDS = ['username']
+
+    # Метод str для строкового представления объекта CustomUser
+    def __str__(self):
+        return self.username
