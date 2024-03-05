43ed2a461e77cf3d5ca5dd2489de5990a7c3d64d
alexandr.lobanov <alexandp83@mail.ru>
2024-03-05 22:19:12 +0300

    Реализована возможность работы с продуктами в админке

--- exercise_2/food_store/admin.py
+++ exercise_2/food_store/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Category, SubCategory
+from .models import Category, SubCategory, Product
 
 admin.site.register(Category)
 admin.site.register(SubCategory)
-
+admin.site.register(Product)
 
 
 
