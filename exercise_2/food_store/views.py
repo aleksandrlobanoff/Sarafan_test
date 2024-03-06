17862e9efa1d3b88d7147cc8b43565da622bb4d1
alexandr.lobanov <alexandp83@mail.ru>
2024-03-06 15:09:23 +0300

    Созданы CRUD для Категорий

--- exercise_2/food_store/views.py
+++ exercise_2/food_store/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Category
+from food_store.serializers import CategorySerializer
 
-# Create your views here.
+
+
+class CategoryListAPIView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    pagination_class = CategoryPagination  # Использование класса пагинации для разбивки списка категорий на страницы
+
+
+class CreateCategoryAPIView(generics.CreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class EditCategoryAPIView(generics.UpdateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = 'id'  # Поле для поиска существующей категории по идентификатору
+
+
+class DeleteCategoryAPIView(generics.DestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = 'id'  # Поле для поиска существующей категории по идентификатору
