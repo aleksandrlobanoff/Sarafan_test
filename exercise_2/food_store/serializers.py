b184fe7c5b2d2f5312395defa78ab61b59fb8afa
alexandr.lobanov <alexandp83@mail.ru>
2024-03-06 15:19:54 +0300

    Добавлен сериализатор product

--- exercise_2/food_store/serializers.py
+++ exercise_2/food_store/serializers.py
@@ -1,7 +1,28 @@
 from rest_framework import serializers
-from food_store.models import Category
+from food_store.models import Category, Product
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.SerializerMethodField()
+    subcategory = serializers.SerializerMethodField()
+    images = serializers.SerializerMethodField()
+
+    def get_category(self, obj):
+        return obj.subcategory.category.name
+
+    def get_subcategory(self, obj):
+        return obj.subcategory.name
+
+    def get_images(self, obj):
+        images = [obj.image1, obj.image2, obj.image3]
+        return [image.url if image else None for image in images]
+
+    class Meta:
+        model = Product
+        fields = ['name', 'slug', 'category', 'subcategory', 'price', 'images']
