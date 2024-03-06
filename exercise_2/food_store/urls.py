334e2f812a6dfb0ba87002df085a741180c38856
alexandr.lobanov <alexandp83@mail.ru>
2024-03-06 15:26:55 +0300

    Добавлены эндпоинты для CRUD Categories

--- exercise_2/food_store/urls.py
+++ exercise_2/food_store/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-
+from . import views
 from food_store.apps import FoodStoreConfig
 
 app_name = FoodStoreConfig.name
 
 urlpatterns = [
-
+    path('categories/', views.CategoryListAPIView.as_view(), name='category-list'),
+    path('category/create/', views.CreateCategoryAPIView.as_view(), name='create-category'),
+    path('category/edit/<int:pk>/', views.EditCategoryAPIView.as_view(), name='edit-category'),
+    path('category/delete/<int:pk>/', views.DeleteCategoryAPIView.as_view(), name='delete-category'),
 ]
